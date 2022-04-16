06147307e71c6f469641a2c516eaa55bdcab5f0c
Emmanuel <emmopk11@outlook.com>
2022-04-16 02:16:18 +0200

    1.2 course information, step2 complete

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,21 +3,27 @@ const Header = (props) => {
     return  <h1>{props.course}</h1>
 }
 
+const Part = ({part, exercises}) => {
+    return <p> {part} {exercises}</p>
+}
+
 const Content = (props) => {
     const {part1, part2, part3, exercises1, exercises2, exercises3} = props
 
     return (
-        <>
-          <p> {part1} {exercises1}</p>
-          <p> {part2} {exercises2}</p>
-          <p> {part3} {exercises3}</p>
-        </>
+        <div>
+            <Part part={part1} exercises={exercises1}/>
+            <Part part={part2} exercises={exercises2}/>
+            <Part part={part3} exercises={exercises3}/>
+        </div>
     )
 }
 
 const Total = (props) => {
     return <p>Number of exercises {props.total}</p>
 }
+
+
 
 function App() {
   const course = 'Half Stack application development'
