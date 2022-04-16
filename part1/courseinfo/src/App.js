a14788922afc29c1a23517a4e6c0031921dff244
Emmanuel <emmopk11@outlook.com>
2022-04-16 23:19:40 +0200

    1.3: course information step3 complete

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,18 +3,18 @@ const Header = (props) => {
     return  <h1>{props.course}</h1>
 }
 
-const Part = ({part, exercises}) => {
-    return <p> {part} {exercises}</p>
+const Part = ({name, exercises}) => {
+    return <p> {name} {exercises}</p>
 }
 
 const Content = (props) => {
-    const {part1, part2, part3, exercises1, exercises2, exercises3} = props
+    const {part1, part2, part3} = props
 
     return (
         <div>
-            <Part part={part1} exercises={exercises1}/>
-            <Part part={part2} exercises={exercises2}/>
-            <Part part={part3} exercises={exercises3}/>
+            <Part {...part1}/>
+            <Part {...part2} />
+            <Part {...part3}/>
         </div>
     )
 }
@@ -27,12 +27,10 @@ const Total = (props) => {
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {name: 'Fundamentals of React', exercises: 10}
+  const part2 = {name: 'Using props to pass data', exercises: 7}
+  const part3 = {name: 'State of a component', exercises: 14}
+
 
   return (
     <div>
@@ -41,11 +39,8 @@ function App() {
             part1={part1}
             part2={part2}
             part3={part3}
-            exercises1={exercises1}
-            exercises2={exercises2}
-            exercises3={exercises3}
         />
-        <Total total={exercises1 + exercises2 + exercises3} />
+        <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 }
