8c7320b94b98787a3a1e8776eed2daf79512bd61
Emmanuel <emmopk11@outlook.com>
2022-04-18 00:28:38 +0200

    2.5: separate module complete

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,38 @@
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part name={part.name} exercises={part.exercises} key={part.id} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ total }) => {
+  return <b>Number of exercises {total}</b>;
+};
+
+const Course = ({ course }) => {
+  const total = course.parts.reduce((sum, curr) => sum + curr.exercises, 0);
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total total={total} />
+    </div>
+  );
+};
+
+export default Course;
